5c7f66873acf2526971539b62b0414255b01505b
DevEmad007 <farshidibtihaj@gmail.com>
2023-09-01 01:09:41 +0600

    responsive navigation bar

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const toggleMenu = document.querySelector('.toggleMenu');
+const menuToggle = document.querySelector('.menuToggle');
 const header = document.querySelector('header');
-const navList = document.querySelector('.navList');
+const navList = document.querySelector('nav');
 
-toggleMenu.addEventListener('click',() => {
+menuToggle.addEventListener('click',() => {
+    console.log(99);
     const classList = navList.classList;
     if (!classList.contains('showMenu')) {
         classList.add('showMenu');
